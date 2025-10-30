ffc57aedefde56a98e866ea4c465d903a46b5596
Vitech <i@vitechliu.com>
2025-10-30 21:35:39 +0800

    0.2.1 词缀属性浮动

--- VTLib/VTLib.cs
+++ VTLib/VTLib.cs
@@ -1,9 +1,9 @@
 namespace VTModifiers.VTLib;
 
 public class VTLib {
-    public static Dictionary<string, object> loadConfig(string path) {
-        if (File.Exists(path)) {
-            
-        }
-    }
+    // public static Dictionary<string, object> loadConfig(string path) {
+    //     if (File.Exists(path)) {
+    //         
+    //     }
+    // }
 }
