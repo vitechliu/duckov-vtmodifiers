82d7bcbc68689fc04a332933aeb5d23d84d05aa0
vitech <i@vitechliu.com>
2025-10-31 19:59:51 +0800

    tag

--- VTLib/VTLib.cs
+++ VTLib/VTLib.cs
@@ -1,3 +1,6 @@
+using Duckov.Utilities;
+using ItemStatsSystem;
+
 namespace VTModifiers.VTLib;
 
 public static class VTLib {
@@ -9,5 +12,14 @@ public static class VTLib {
     
     public static bool Probability(float probability) {
         return UnityEngine.Random.value < probability;
+    }
+
+    public static string DebugItemTags(Item item) {
+        string res = "";
+        foreach (Tag tag in item.Tags) {
+            string dd = tag.Show ? "(SHOW)" : "";
+            res += $"[{tag.DisplayName}{dd}]";
+        }
+        return res;
     }
 }
